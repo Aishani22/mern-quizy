86c88a184a71949af3737c75bf4597c10296b3c4
Aishani <Aishani@example.com>
2025-08-12 16:28:37 +0530

    Commiting CSS changes

--- client/src/pages/LoaderContext.jsx
+++ client/src/pages/LoaderContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useContext } from 'react';
-import Loader from './Loader';
+import Loader from "../components/Loader.jsx";
 
 const LoaderContext = createContext();
 
